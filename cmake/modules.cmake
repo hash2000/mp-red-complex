193b9c9c036bab868e2cb90e2bca869a877150e5
Hash2000 <infinite_2000@mail.ru>
2025-10-29 21:00:59 +0300

    base configuration the Launcher application with Qt ui

---
+++ cmake/modules.cmake
@@ -0,0 +1,19 @@
+
+find_package(Poco
+    CONFIG REQUIRED COMPONENTS
+        Foundation
+        XML
+        JSON
+        Util
+        Net
+		NetSSL
+        Crypto
+        JWT
+        Encodings)
+
+
+find_package(Qt6
+	REQUIRED COMPONENTS
+	Core
+	Gui
+	Widgets)
