20fe6852328d664e4668b20f1b6cb9c23d49af17
Hash2000 <infinite_2000@mail.ru>
2025-11-05 12:04:39 +0300

    ResourcesView markup

--- src/Launcher/main_frame/resources_viewer/resources_viewer_frame.cpp
+++ src/Launcher/main_frame/resources_viewer/resources_viewer_frame.cpp
@@ -1,5 +1,66 @@
 #include "Launcher/main_frame/resources_viewer/resources_viewer_frame.h"
+#include "resources_viewer_frame.h"
+#include <QSplitter>
+#include <QFileInfo>
+#include <QMessageBox>
+#include <QInputDialog>
+#include <QPushButton>
 
 ResourcesViewerFrame::ResourcesViewerFrame() {
+	setupAssetsTree();
+	setupCentralWidget();
+	setupActionPanel();
 
+
+
+	QSplitter *splitter = new QSplitter(Qt::Horizontal);
+  splitter->addWidget(_assetsView);
+  splitter->addWidget(_centerStack);
+	splitter->addWidget(_actionsPanel);
+  splitter->setStretchFactor(0, 1);
+  splitter->setStretchFactor(1, 2);
+	splitter->setStretchFactor(2, 3);
+
+  setCentralWidget(splitter);
+  setWindowTitle("Asset Manager");
+	populateAssetsView();
+}
+
+void ResourcesViewerFrame::setupCentralWidget() {
+	_centerStack = new QStackedWidget;
+  _textEditor = new QTextEdit;
+  _textEditor->setReadOnly(true);
+  _imageLabel = new QLabel;
+  _imageLabel->setAlignment(Qt::AlignCenter);
+  _emptyWidget = new QWidget;
+
+	_centerStack->addWidget(_emptyWidget);
+  _centerStack->addWidget(_textEditor);
+  _centerStack->addWidget(_imageLabel);
+}
+
+void ResourcesViewerFrame::populateAssetsView() {
+
+}
+
+void ResourcesViewerFrame::setupAssetsTree() {
+	_assetsModel = new QStandardItemModel(this);
+  _assetsModel->setHorizontalHeaderLabels({"Assets"});
+
+	_assetsView = new QTreeView;
+  _assetsView->setModel(_assetsModel);
+  _assetsView->setContextMenuPolicy(Qt::CustomContextMenu);
+  _assetsView->setHeaderHidden(true);
+}
+
+void ResourcesViewerFrame::setupActionPanel() {
+	_actionsPanel = new QWidget;
+  _actionsLayout = new QVBoxLayout;
+  _actionsPanel->setLayout(_actionsLayout);
+
+  _actionsLayout->addWidget(new QLabel("<b>Actions:</b>"));
+  _actionsLayout->addWidget(new QPushButton("Edit"));
+  _actionsLayout->addWidget(new QPushButton("Delete"));
+  _actionsLayout->addWidget(new QPushButton("Export"));
+  _actionsLayout->addStretch();
 }
--- src/Launcher/main_frame/resources_viewer/resources_viewer_frame.h
+++ src/Launcher/main_frame/resources_viewer/resources_viewer_frame.h
@@ -1,5 +1,14 @@
 #pragma once
 #include "Launcher/main_frame.h"
+#include <QMainWindow>
+#include <QTreeView>
+#include <QStandardItemModel>
+#include <QStackedWidget>
+#include <QTextEdit>
+#include <QLabel>
+#include <QMenu>
+#include <QVBoxLayout>
+#include <QListWidget>
 
 class ResourcesViewerFrame : public MainFrame {
 public:
@@ -7,5 +16,26 @@ public:
 
 	virtual ~ResourcesViewerFrame() = default;
 
+private:
+	void setupActionPanel();
+	void setupAssetsTree();
+	void setupCentralWidget();
+
+	void populateAssetsView();
+
+private:
+	QTreeView *_assetsView;
+	QStandardItemModel *_assetsModel;
+
+	QStackedWidget *_centerStack;
+  QTextEdit *_textEditor;
+  QLabel *_imageLabel;
+  QWidget *_emptyWidget;
+
+  QStandardItem *_currentItem;
+
+	QListWidget *_actionsList;
+  QVBoxLayout *_actionsLayout;
+  QWidget *_actionsPanel;
 };
 
