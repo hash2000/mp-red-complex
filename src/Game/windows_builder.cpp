ab9ef8ee3e1bf72da8fb3e81dfb95c3e2e7b0460
Hash2000 <infinite_2000@mail.ru>
2026-03-09 20:50:55 +0300

    fix WindowsBuilder

--- src/Game/windows_builder.cpp
+++ src/Game/windows_builder.cpp
@@ -6,6 +6,8 @@
 #include "Game/widgets/equipment/equipment_window.h"
 #include "Game/widgets/inventory/inventory_window.h"
 #include "Game/widgets/items/entities_window.h"
+#include <map>
+#include <functional>
 
 class WindowsBuilder::Private {
 public:
@@ -15,38 +17,43 @@ public:
 
 	WindowsBuilder* q;
 	ApplicationController* appController;
+	std::map<QString, std::function<MdiChildWindow*(Services*, const QString&)>> factory;
 };
 
 WindowsBuilder::WindowsBuilder(ApplicationController* appController)
-	: d(std::make_unique<Private>(this)) {
+: d(std::make_unique<Private>(this)) {
 	d->appController = appController;
+
+	d->factory.emplace("map", [](Services* services, const QString& id) {
+		return new MapWindow(
+			services->worldService(),
+			services->timeService(),
+			id);
+		});
+	d->factory.emplace("equipment", [](Services* services, const QString& id) {
+		return new EquipmentWindow(
+			services->inventoriesService(),
+			id);
+		});
+	d->factory.emplace("inventory", [](Services* services, const QString& id) {
+		return new InventoryWindow(
+			services->inventoriesService(),
+			id);
+		});
+	d->factory.emplace("items", [](Services* services, const QString& id) {
+		return new EntitiesWindow(
+			services->itemsService(),
+			id);
+		});
 }
 
 WindowsBuilder::~WindowsBuilder() = default;
 
 MdiChildWindow* WindowsBuilder::build(const QString& name, const QString& id) {
-
-	if (name == "map") {
-		return new MapWindow(
-			d->appController->services()->worldService(),
-			d->appController->services()->timeService(),
-			id);
-	}
-	else if (name == "equipment") {
-		return new EquipmentWindow(
-			d->appController->services()->inventoriesService(),
-			id);
-	}
-	else if (name == "inventory") {
-		return new InventoryWindow(
-			d->appController->services()->inventoriesService(),
-			id);
-	}
-	else if (name == "items") {
-		return new EntitiesWindow(
-			d->appController->services()->itemsService(),
-			id);
+	const auto &it = d->factory.find(name);
+	if (it == d->factory.end()) {
+		return nullptr;
 	}
 
-	return nullptr;
+	return it->second(d->appController->services(), id);
 }
